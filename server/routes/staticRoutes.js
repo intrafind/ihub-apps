80e0823b1d1317115a20621ee8eca14d1a8265b7
Daniel Manzke <manzke@users.noreply.github.com>
2025-06-07 18:49:04 +0200

    fix: serve static files from correct directory

--- server/routes/staticRoutes.js
+++ server/routes/staticRoutes.js
@@ -10,9 +10,9 @@ export default function registerStaticRoutes(app, { isPackaged, rootDir }) {
   if (isPackaged) {
     staticPath = path.join(rootDir, 'public');
   } else if (process.env.NODE_ENV === 'production') {
-    staticPath = path.join(__dirname, '../public');
+    staticPath = path.join(rootDir, 'public');
   } else {
-    staticPath = path.join(__dirname, '../client/dist');
+    staticPath = path.join(rootDir, 'client/dist');
   }
 
   console.log(`Serving static files from: ${staticPath}`);
@@ -23,9 +23,9 @@ export default function registerStaticRoutes(app, { isPackaged, rootDir }) {
     if (isPackaged) {
       indexPath = path.join(rootDir, 'public/index.html');
     } else if (process.env.NODE_ENV === 'production') {
-      indexPath = path.join(__dirname, '../public/index.html');
+      indexPath = path.join(rootDir, 'public/index.html');
     } else {
-      indexPath = path.join(__dirname, '../client/dist/index.html');
+      indexPath = path.join(rootDir, 'client/dist/index.html');
     }
     console.log(`Serving SPA from: ${indexPath}`);
     res.sendFile(indexPath);
