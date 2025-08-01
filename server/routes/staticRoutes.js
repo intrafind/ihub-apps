409d5af5a66ab2ebbfbb3af131a57b3c7a6f7c06
Daniel Manzke <daniel.manzke@intrafind.com>
2025-08-01 13:17:32 +0200

    git status

--- server/routes/staticRoutes.js
+++ server/routes/staticRoutes.js
@@ -23,16 +23,18 @@ export default function registerStaticRoutes(app, { isPackaged, rootDir }) {
   console.log(`Serving uploaded assets from: ${uploadsPath}`);
   app.use('/uploads', express.static(uploadsPath));
 
+  // Determine index path once during setup
+  let indexPath;
+  if (isPackaged) {
+    indexPath = path.join(rootDir, 'public/index.html');
+  } else if (config.NODE_ENV === 'production') {
+    indexPath = path.join(rootDir, 'public/index.html');
+  } else {
+    indexPath = path.join(rootDir, 'client/dist/index.html');
+  }
+  console.log(`SPA will be served from: ${indexPath}`);
+
   app.get('*', (req, res) => {
-    let indexPath;
-    if (isPackaged) {
-      indexPath = path.join(rootDir, 'public/index.html');
-    } else if (config.NODE_ENV === 'production') {
-      indexPath = path.join(rootDir, 'public/index.html');
-    } else {
-      indexPath = path.join(rootDir, 'client/dist/index.html');
-    }
-    console.log(`Serving SPA from: ${indexPath}`);
     res.sendFile(indexPath);
   });
 }
